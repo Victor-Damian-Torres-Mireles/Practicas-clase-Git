ff9e96c51bbfec875c12b31da5d6e45bea4ea424
Victor-Damian-Torres-Mireles <2030019@upv.edu.mx>
2026-09-23 15:52:22 -0600

    23/09/26

---
+++ understanding_variables/src/Understanding_Numbers.py
@@ -0,0 +1,18 @@
+# Numeros
+# Enteros - Integers
+
+"""
+Los numeros enteros los podemos sumar (+), restar (-),
+multiplicar (*) y dividir (/).
+"""
+
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+
+number_1 = 5
+number_2 = 10
+print(number_1+number_2)
+
+"sea number 1 =5 el metodo title lo puedo aplicar a la variable number ? NO"
---
+++ understanding_variables/src/Understanding_Sintax_Error.py
@@ -0,0 +1,19 @@
+# Eliminacion de espacios en blanco
+programming_languaje = "         Python            "
+print (programming_languaje)
+print ("Efecto del lstrip")
+print (programming_languaje.lstrip())
+print ("Efecto del rstrip")
+print (programming_languaje.rstrip())
+print ("Efecto del strip")
+print (programming_languaje.strip())
+print ("Variable")
+print (programming_languaje)
+
+print ("------------------------")
+
+# Error de Sintaxis
+message = 'Una fortaleza de python es su comunidad'
+print (message)
+message = 'Una fortaleza de "python" es su comunidad'
+print (message)
